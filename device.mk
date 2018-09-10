4e4034c45a801fe33853e24e7e3fd5947aacf135
relikvent-som <39597547+relikvent-som@users.noreply.github.com>
2018-09-10 11:56:19 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -42,9 +42,8 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.oxygen.rc \
-    init.goodix.sh
-
+    init.oxygen.rc
+    
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
