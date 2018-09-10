2c29966df3c6e8bf1d7f9af5e540f022f1624a78
relikvent-som <39597547+relikvent-som@users.noreply.github.com>
2018-09-10 11:59:34 +0530

    Update lineage_oxygen.mk

--- lineage_oxygen.mk
+++ lineage_oxygen.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from oxygen device
-$(call inherit-product, device/xiaomi/oxygen/device.mk)
+$(call inherit-product, device/xiaomi/oxygen/full_oxygen.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
